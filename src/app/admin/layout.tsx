82bdfd3d6a8b3e3316d77eb4c60fbe3318db97e0
Hunter Haufler <haufhun@gmail.com>
2025-03-06 07:39:44 -0600

    Authorization and Routes Protection in NextJS

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,4 @@
-export default function RootLayout({
+export default function AdminLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   // TODO: Check if user is authenticated and is admin
---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,29 @@
+import { createClient } from "@/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const supabase = await createClient();
+
+  const { data: authData } = await supabase.auth.getUser();
+
+  if (authData.user) {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", authData.user.id)
+      .single();
+
+    if (error || !data) {
+      console.log("Error fetching user data", error);
+      return;
+    }
+
+    if (data.type === "admin") {
+      return redirect("/admin");
+    }
+  }
+
+  return <>{children}</>;
+}
