d27c1bf5258d29c6a91472c5ca5a98ba74f7a1b0
Hunter Haufler <haufhun@gmail.com>
2025-04-09 07:36:16 -0500

    Product Page Setup

--- src/app/admin/categories/categories.types.ts
+++ src/app/admin/categories/categories.types.ts
@@ -3,11 +3,19 @@ export type Product = {
   created_at: string;
   hero_image: string;
   id: number;
-  imageurl: string[];
+  image_url: string[];
   maxquantity: number;
   price: number | null;
   slug: string;
   title: string;
+};
+
+export type Category = {
+  created_at: string;
+  id: number;
+  image_url: string;
+  name: string;
+  slug: string;
 };
 
 export type CategoryWithProducts = {
---
+++ src/app/admin/products/page-component.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import { Category } from "@/app/admin/categories/categories.types";
+
+type Props = {
+  categories: Category[];
+};
+
+export const ProductPageComponent: FC<Props> = ({ categories }) => {
+  return <div>ProductComponent</div>;
+};
