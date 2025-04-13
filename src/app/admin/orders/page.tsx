972e9d25b743c9ed9c9fb939408bb727260fa646
Hunter Haufler <haufhun@gmail.com>
2025-04-13 11:56:27 -0500

    Orders Page

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,6 @@
+export default async function Orders() {
+  // const categories = await getCategoriesWithProducts();
+  // const productsWithCategories = await getProductsWithCategories();
+
+  return <div>Orders Page</div>;
+}
